30fb5f9b046e344472c0835804ce12fb6439b757
Itz Cook1e <Asherpflueger@outlook.com>
2021-10-03 21:42:04 -0500

    Added function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # Average them. (Hint: You haven't been taught how to create an unknown number of variables, so I don't expect you to create grade1, grade2, etc. Just get a new grade each time through the loop, print it, and add it to a running total. When the loop is done, you should have a total of all of the grades, and simply divide that by the number of grades to get an average.)
 # Print the average and its letter grade.
 # Don't forget to put your name and date comment, as well as any other comments that will explain what you do!
+# Was instructed to re-do as I did not include a function. One has now been added.
 
 # Importing libraries
 import random
@@ -39,31 +40,35 @@ for i in range(gradeamount):
     # Fill list (empty list above)
     grades.append(grade)
 
-    if grade > 90:
-        givengrade = "A"
-    elif 80 < grade <= 90:
-        givengrade = "B"
-    elif 70 < grade <= 80:
-        givengrade = "C"
-    elif 60 < grade <= 70:
-        givengrade = "D"
+    if grade >= 90:
+        givengrade = 'A'
+    elif 89 > grade >= 80:
+        givengrade = 'B'
+    elif 79 > grade >= 70:
+        givengrade = 'C'
+    elif 69 > grade >= 60:
+        givengrade = 'D'
     else:
-        givengrade = "F"
-    print(f'Grade: {grade} - {givengrade}')
+        givengrade = 'F'
+    print(f'Grade: {grade} {givengrade}')
 
 # Calculate average grade(s)
-avg_grades = sum(grades)/gradeamount
-if avg_grades > 90:
-    givenavggrade = "A"
-elif 80 < avg_grades <= 90:
-    givenavggrade = "B"
-elif 70 < avg_grades <= 80:
-    givenavggrade = "C"
-elif 60 < avg_grades <= 70:
-    givenavggrade = "D"
-else:
-    givenavggrade = "F"
-print()
-print(f'Average: {avg_grades} - {givenavggrade}')
+avg_grades = sum(grades) / gradeamount
 
+
+def average_grade_letter(grade):
+    if avg_grades >= 90:
+        return 'A'
+    elif 89 > avg_grades >= 80:
+        return 'B'
+    elif 79 > avg_grades >= 70:
+        return 'C'
+    elif 69 > avg_grades >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+
+print()
+print(f'Average Grade: {avg_grades} {average_grade_letter(avg_grades)}')
 # Comment with name, date, and assignment name redacted
